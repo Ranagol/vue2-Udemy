bedd11a39f31902781bcde66052546c032b90b5d
Andor <andorhorvat@gmail.com>
2020-01-07 17:33:23 +0100

    39

---
+++ 036-039-v-if_v-show/main.js
@@ -0,0 +1,25 @@
+new Vue({
+  el:'#app',
+
+  data: {
+    show: true,
+  },
+
+
+});
+
+/*
+TEMPLATE FOR v-if, v-else-if, v-else
+<div v-if="type === 'A'">
+  A
+</div>
+<div v-else-if="type === 'B'">
+  B
+</div>
+<div v-else-if="type === 'C'">
+  C
+</div>
+<div v-else>
+  Not A/B/C
+</div>
+*/
