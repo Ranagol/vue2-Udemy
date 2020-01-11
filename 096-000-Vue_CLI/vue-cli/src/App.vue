595dec35b84e2c865f23fe3ed84247fef7e97053
Andor <andorhorvat@gmail.com>
2020-01-11 15:11:16 +0100

    96

---
+++ 096-000-Vue_CLI/vue-cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Server status: {{ status }}</p>
+</template>
+
+<script>
+export default {//here we are exporting an object. Which will contain our data. Data has to be stored in a form of a function, in order to work. All this will be the root Vue instance, which will be rendered by the render: h => h(App).
+  data: function(){
+    return {
+      status: 'Critical'
+    }
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
