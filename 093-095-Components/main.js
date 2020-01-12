fb093c296c68725f2923b22173c5d83eb6384b05
Andor <andorhorvat@gmail.com>
2020-01-12 10:40:22 +0100

    104

--- 093-095-Components/main.js
+++ 093-095-Components/main.js
@@ -1,6 +1,6 @@
 //var data = {status: 'Critical'};
 
-var componentLocal = {//this is how we can do local registering. We use variables, instead of Vue.component
+var componentLocal = {//this is how we can do local registering. We use variables, instead of Vue.component, and we store this component in a variable.
   data: function(){
     return {
       status: 'Critical'
@@ -18,25 +18,23 @@ var componentLocal = {//this is how we can do local registering. We use variable
 
 new Vue({
   el: '#app',
-  components: {//this is the second step of local registration. We add components property to the Vue instance. THese components will be only usable in this isntance. Locally.
+  components: {//this is the second step of local registration. We add components property to the Vue instance. THese components will be only usable in this instance. Locally.
     
-    'my-tag': componentLocal //reminder: componentLocal is our variable, where we store our component
+    'my-tag': componentLocal //reminder: componentLocal is our variable, where we store our component. 'my-tag' will be the tag or selector for this component.
   }
-  
 })
 
 new Vue({
   el: '#app2'
-  
 })
 
 //so. We did select a div app, but we have overwritten all this with our template.
 //we want to put our status to different places in our html. But Vue instance can only work with the first selected element, not more.
 //for this, we must use components. The components extends the Vue instance. Component will have 2 arguments
 //the first argument in the component is the tag name that we will use
-//the second argument is the actual object, that is our component. We can simply copy our data, template, methods... from Vue instance to the component. But data was an object in the Vue instance. It will become a function in the component. This way this won't interfere with our data??
+//the second argument is the actual object, that is our component. We can simply copy our data, template, methods... from Vue instance to the component. 
+//But, data was an object in the Vue instance. It will become a function in the component. This way this won't interfere with our data??
 //However, now we can repeat <myTag></myTag> with our template in it. We could not do that with the original Vue instance.
 //If we click on the Change button, only the belonging status will change from Critical to normal (not both). Why? Because we have two <my-tag></my-tag> component objects, and they are different objects. We acces the status with this, and this referrs to the current object only.
-//we could use the same div selector, but to use different components for it. For this we need to register Vue.component locally.
+//we could use the same div selector, but to use different components for it. For this we need to register Vue.component locally, and not globally. So, there are these two ways of registering a component.
 //Vue.component is globally registering.
-//We can store this the former Vue.component as a variable,
---
+++ 098-Exercise-6/exercise6/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+
+        <my-header></my-header>
+
+        <hr>
+
+        <my-Servers></my-servers>
+
+        <my-server-details></my-server-details>
+        <hr>
+
+        <my-footer></my-footer>
+
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import Servers from './components/Servers.vue';
+    import ServerDetails from './components/ServerDetails.vue';
+
+    export default {
+        components: {
+            'my-header': Header,
+            'my-servers': Servers,
+            'my-server-details': ServerDetails,
+            'my-footer': Footer,
+        }
+    }
+</script>
+
+<style>
+
+</style>
