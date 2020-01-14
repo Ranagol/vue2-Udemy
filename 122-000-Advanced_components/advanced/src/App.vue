af26786fe4aa532aeb6a71e0e30bd2bbd3b77108
Andor <andorhorvat@gmail.com>
2020-01-14 19:23:57 +0100

    126

--- 122-000-Advanced_components/advanced/src/App.vue
+++ 122-000-Advanced_components/advanced/src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <app-quote></app-quote>
+                <app-quote><!--We want to pass this block of code with html tags to the App.vue all together. Solution: slots -->
+                    <h2>{{ quoteTitle }}</h2>
+                    <p>A wonderful quote</p>
+                </app-quote>
             </div>
         </div>
     </div>
@@ -12,6 +15,13 @@
     import Quote from './components/Quote.vue';//1 step of local component registration
     
     export default {
+
+        data: function(){
+            return {
+                quoteTitle: 'The quote',
+            }
+        },
+
         components: {
             appQuote: Quote//we register here locally the Quote.vue (2 step)
         }
@@ -19,4 +29,8 @@
 </script>
 
 <style>
+
+h2{
+  color: red;
+}
 </style>
--- 122-000-Advanced_components/advanced/src/components/Quote.vue
+++ 122-000-Advanced_components/advanced/src/components/Quote.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-      <p>A wonderful quote</p>
+    <slot></slot>
   </div>
 </template>
 
 <script>
+  export default {
+    
+  }
 
 </script>
 
 
 <style scoped>
-/*scoped means that this div style is applied only here in this component*/
+/*scoped means that this div style is applied only here in this component. Basically, all the styling of the child Quote.vue is done in the child Quote.vue, but everything else is done in the parent App.vue*/
 div{
   border: 1px solid #ccc;
   box-shadow: 1px 1px 2px black;
@@ -18,4 +21,6 @@ div{
   margin: 30px auto;
   text-align: center;
 }
+
+
 </style>
