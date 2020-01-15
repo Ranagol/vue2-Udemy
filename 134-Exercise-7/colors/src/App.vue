ba8c91813cad8cc3253d29c2662d37ab7ee46735
Andor <andorhorvat@gmail.com>
2020-01-15 12:29:06 +0100

    134

---
+++ 134-Exercise-7/colors/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <p>{{ selectedComponent }}</p>
+                <hr>
+                <component :is="selectedComponent"></component>
+                <hr>
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function(){
+            return {
+                selectedComponent: 'appBlue',
+            }
+        },
+        
+        
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
