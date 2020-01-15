4fbd26cca289a330c4ab3e68d6058e0af7939b1d
Andor <andorhorvat@gmail.com>
2020-01-15 11:19:24 +0100

    132

--- 122-000-Advanced_components/advanced/src/App.vue
+++ 122-000-Advanced_components/advanced/src/App.vue
@@ -7,20 +7,25 @@
                 <button @click="selectedComponent='appNew'">New</button>
                 <hr>
                 <p>{{ selectedComponent }}</p>
-                <component :is="selectedComponent">
-                    <p>Default component</p>
-                </component><!-- this is a reserved tag for dynamic Vue component, that will allow us to change component display. We binded this component to selectedComponent property with this part: :is="selectedComponent"-->
-                <!--We want to pass this block of code with html tags to the App.vue all together. Solution: slots -->
-                <!--
+
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default component</p>
+                    </component><!-- this is a reserved tag for dynamic Vue component, that will allow us to change component display. We binded this component to selectedComponent property with this part: :is="selectedComponent" -->
+                </keep-alive><!--Problem: the switched component is being destroyed, so it can't remember things. Solution: <keep-alive></keep-alive>-->
+
+
+<!--We want to pass this block of code with html tags to the App.vue all together. Solution: slots -->
+<!--
                 <app-quote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
                     <p>A wonderful quote</p>
                 </app-quote>
-                -->
+-->
             </div>
         </div>
     </div>
-</template><!--We have 3 components. We want to dynamically decide which component we want to display here, depending on which button was pressed. For this, we use dynamic components. -->
+</template><!--We have 3 components: Author, New, Quote. We want to dynamically decide which component we want to display here, depending on which button was pressed. For this, we use dynamic components.  -->
 
 <script>
     import Quote from './components/Quote.vue';//1 step of local component registration
--- 122-000-Advanced_components/advanced/src/components/New.vue
+++ 122-000-Advanced_components/advanced/src/components/New.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
    <h3>New quote</h3>
+   <button @click="counter++">Increase!</button>
+   <p>{{ counter }}</p><!--with this counter we are just checking if the component is destroyed after a component switch. Yes, the component dies after a switch, it can't remember this counter value. The counter is resetted.  -->
   </div>
 </template>
 
 <script>
   export default {
-    
+    data: function(){
+      return{
+        counter:0,
+      };
+    }
   }
 </script>
 
