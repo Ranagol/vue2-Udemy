b48c2586ac8474a606eb5aa2ec2a3f12f026f905
Andor <andorhorvat@gmail.com>
2020-01-16 09:17:46 +0100

    164

--- 163-000-Directives/directives/src/main.js
+++ 163-000-Directives/directives/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight',{//first argument is the name of the directive (without the v- part).
+
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
