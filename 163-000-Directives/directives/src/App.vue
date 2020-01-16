e792d6bd1e563c1f2af8db4caa7babaf0cd2f1a0
Andor <andorhorvat@gmail.com>
2020-01-16 11:32:08 +0100

    170

--- 163-000-Directives/directives/src/App.vue
+++ 163-000-Directives/directives/src/App.vue
@@ -3,23 +3,27 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Built in directives</h1>
-                <p v-text="'Some text'"></p>
+                <p v-text="'Some text'"></p><!--""is needed for every directive. '' shows us that this is a string. -->
                 <p v-html="'<strong>Some strong text</strong>'"></p>
             </div>
             <hr>
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom directives</h1>
-                <p>Color this</p><!--We want to add here a custom directive, that will do some background coloring. Obviously this could be done with CSS, but this is just a simple example.-->
+                <p v-highlight:background.delayed="'red'">Color this</p>
+<!--
+1. We want to add here a custom directive, that will do some background coloring. Obviously this could be done with CSS, but this is just a simple example. 
+2. We want the color not to be harcoded, we want to select the color.
+3. We want to pass argument to the directive. User has to decide if he wants to change the backgroundColor, otherwise the text color will be changed. The :background will be the argument. The :backgorund is also a binding, it is classic v-bind.
+4. We want to add a modifier. Example: we want to wait 3 seconds, when the style changes. We use the .delayed modifier(which we will create, this is not a built in modifier).-->
             </div>            
         </div>
     </div>
+<!--We have two options how to do custom directives. 1-global registering. -->
 </template>
 
 <script>
-    export default {
-    }
+    export default {}
 </script>
 
 <style>
-
 </style>
--- 163-000-Directives/directives/src/main.js
+++ 163-000-Directives/directives/src/main.js
@@ -1,10 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highlight',{//first argument is the name of the directive (without the v- part).
+//We are globally registering our customer directive here, below:
 
+Vue.directive('highlight',{//first argument is the name of the directive (without the v- part). binding is our v-highlight. .value is whatever we put into the v-highlight=" ".
+
+  bind(el, binding, vnode){
+    var delay = o;//delay time by default is 0
+
+    if(binding.modifiers['delayed']){//we want to check if our binding has modifiers array (this is always an array), and we want to see if this modifiers array contains 'delayed'. If so, then...
+
+      delay = 3000;//...set the delay time to 3 seconds. This allows us to se a timer...
+    }
+
+    setTimeout(() => {// this is the callback function:() =>...
+      if(binding.arg == 'background'){//if the binding argument is background...
+      
+        el.style.backgroundColor = binding.value;//...the elements CSS style backgroundColor should be = with the binded value. .value is whatever we put into the v-highlight=" "
+  
+      } else {
+        el.style.color = binding.value;//...otherwise, set the text color (not the backgorund color!) = binded value. So, if there is no :background, make the text colored, according to the binded value. .value is whatever we put into the v-highlight=" "
+      }
+    }, delay);
+  }
 });
-
+/*
+for a directive configuring we have 5 hooks, similar like the lifecycle hooks
+1-bind() when the directive is attached, applied
+2-inserted() when it is inserted into the DOM
+3-update() when the component is updated, without children
+4-componentUpdated() when the component is updated, with children
+5-unbind() when the directive is removed
+*/
 new Vue({
   el: '#app',
   render: h => h(App)
