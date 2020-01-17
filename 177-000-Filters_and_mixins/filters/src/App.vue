8c28e889ea341129e8b0353f22ea95be9ed10522
Andor <andorhorvat@gmail.com>
2020-01-17 18:30:47 +0100

    180

--- 177-000-Filters_and_mixins/filters/src/App.vue
+++ 177-000-Filters_and_mixins/filters/src/App.vue
@@ -6,7 +6,19 @@
                 <p>Our text without filtering: {{ text}}</p>
                 <p>Our text with the toUppercase filter: {{ text | toUppercase }}</p><!-- | toUppercase this is how we activate our filter on the text data. -->
                 <p>Our text with multiple filters: {{ text | toUppercase | toLowercase }}</p><!-- Here we are using multiple filters. This is how we can chain filters-->
-
+                <hr>
+                <h3>Filtering with computed properties</h3>
+                <!--Here we want to output our fruits as a list, and then filter it with an input field. -->
+                <input v-model="filterText"><!--This will be used for filtering the fruits. Example: if we enter the letter 'm', we want to see only the fruits, that start with 'm'. Problem: Vue will activate the filter after every change in the DOM, that demands resources. The computed property way is better solution. -->
+                <p>This is the unfiltered version of fruits</p>
+                <ul>
+                    <li v-for="fruit in fruits">{{ fruit }}</li>
+                </ul>
+                <p>This is the filtered version of fruits</p>
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+                
             </div>
         </div>
     </div>
@@ -17,7 +29,9 @@
     export default {
         data(){
             return{
-                text: 'Hello there!'
+                text: 'Hello there!',
+                fruits: ['apple', 'banana', 'mango', 'melon'],
+                filterText: '',//this is empty at the beginning
             }
         },
 
@@ -26,8 +40,15 @@
                 
                 return value.toUpperCase();// toUpperCase is the filter name. toUpperCase() is a built in method in JS. The similarity is a stupid coincidence, nothing more.
             }
-
         },
+
+        computed: {
+            filteredFruits(){
+                return this.fruits.filter((element) => {//filter() is a built in JS function. ()=> is a callback function.
+                    return element.match(this.filterText) //in this callback we must return all the elements that we want to be filtered. So what we are saying here: return all elements that match the filterText. match() is a built in JS function.
+                });
+            }
+        }
     }
 </script>
 
