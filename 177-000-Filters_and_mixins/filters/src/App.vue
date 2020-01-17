4d5107135624043fe84a126af3218f0fd1d25bad
Andor <andorhorvat@gmail.com>
2020-01-17 17:42:35 +0100

    179

--- 177-000-Filters_and_mixins/filters/src/App.vue
+++ 177-000-Filters_and_mixins/filters/src/App.vue
@@ -3,7 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters and mixins</h1>
-                <p>{{ text | toUppercase }}</p><!-- | toUppercase this is how we activate our filter on the text data. -->
+                <p>Our text without filtering: {{ text}}</p>
+                <p>Our text with the toUppercase filter: {{ text | toUppercase }}</p><!-- | toUppercase this is how we activate our filter on the text data. -->
+                <p>Our text with multiple filters: {{ text | toUppercase | toLowercase }}</p><!-- Here we are using multiple filters. This is how we can chain filters-->
 
             </div>
         </div>
@@ -11,7 +13,7 @@
 </template>
 
 <script>
-//a filter can transform some outout into a template. Example, a filter takes a lowercase string and transforms it into uppercase string. Our filter should do this.
+//a filter can transform some output into a template. Example, a filter takes a lowercase string and transforms it into uppercase string. Our filter should do this.
     export default {
         data(){
             return{
@@ -22,7 +24,7 @@
         filters:{
             toUppercase(value){//this will register our filter locally, so we will be able to use this filter only in this components template. The value argument is the data that we want to transform.
                 
-                return value.toUpperCase();// toUppercase is the filter name. toUppercase() is a built in method in JS. The similarity is a stupid coincidence, nothing more.
+                return value.toUpperCase();// toUpperCase is the filter name. toUpperCase() is a built in method in JS. The similarity is a stupid coincidence, nothing more.
             }
 
         },
--- 177-000-Filters_and_mixins/filters/src/main.js
+++ 177-000-Filters_and_mixins/filters/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //We can also register filters globally. Like this:
-Vue.filter('to-lowercase');//to-lowercase will be the name of the globally registered filter
+Vue.filter('toLowercase', function(value){//to-lowercase will be the name of the globally registered filter
+  return value.toLowerCase();//toLowerCase is a built in JS function
+});
 
 new Vue({
   el: '#app',
