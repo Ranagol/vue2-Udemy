34dc22e1231fc7767394bf1fd0b945abfa95fb47
Andor <andorhorvat@gmail.com>
2020-02-09 11:49:48 +0100

    241

--- 233-000-Routing/routing/src/components/Header.vue
+++ 233-000-Routing/routing/src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <ul class="nav nav-pills">
+    <div>This is the Header.vue</div>
     <router-link to="/" tag="li" active-class="active" exact ><a>Home</a></router-link><!--<router-link></router-link> is built in in Vue. tag="li" means that Vue will create an li item. -->
-    <router-link to="/user/10" tag="li" active-class="active" ><a>User</a></router-link>
+    <div>|</div>
+    <router-link to="/user/1" tag="li" active-class="active" ><a>User 1</a></router-link>
+    <div>|</div>
+    <router-link to="/user/2" tag="li" active-class="active" ><a>User 2</a></router-link>
   </ul>
   
 </template>
--- 233-000-Routing/routing/src/components/user/User.vue
+++ 233-000-Routing/routing/src/components/user/User.vue
@@ -14,15 +14,19 @@ export default {
     data(){
             return {
                 //this here is about how to fetch the data from the url
-                id: this.$route.params.id,//the id will the this. Vue instance's $route object's paramter's (yes, the $route object has a params object in it...) id, (which will be passed thorugh the url)
+                id: this.$route.params.id,//the id will the this. Vue instance's $route object's paramter's (yes, the $route object has a params object in it...) id, (which will be passed thorugh the url). Basically we are fetching an id number from the url, putting it into an id property. This id property can be {{}}
+                //problem with this id: if we have user id 1 and user id 2, this id property is not updated accordingly. This is because the User.vue component is not updated/refreshed. Id must be updated, if it changes! Solution: set up a watcher.
             }
         },
+    
+    watch: {
+        '$route'(to, from){//This is a function.Whenever the $route changes(example from /user/1 to user/2), update the id. (to, from) means from which route are we coming and to wich route are we going. The new params from the $route.params.id already has the new route in it.
 
-
+            this.id = to.params.id;//id from the data/return (from above) = id from the new route
+        } 
+    },
 
     methods: {
-        
-
         navigateToHome(){
             this.$router.push('/'); //we are accesing this Vue instance (with this), and there we can acces our Vue router ($router). '/' = because it means url for the home link
         }
